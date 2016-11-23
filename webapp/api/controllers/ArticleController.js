136b48886c8a743a29460e8df54d26e10b630434
samudurand <samueld@cakesolutions.net>
2016-11-23 16:55:32 +0000

    add basic article controller and model

---
+++ webapp/api/controllers/ArticleController.js
@@ -0,0 +1,11 @@
+/**
+ * ArticleController
+ *
+ * @description :: Server-side logic for managing articles
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
