5bcc926f390a7ec1e240dfec110dbf1d023ad1b2
samudurand <samueld@cakesolutions.net>
2016-11-24 15:35:07 +0000

    get all paths

--- webapp/routes/articles.route.js
+++ webapp/routes/articles.route.js
@@ -4,15 +4,33 @@ const Status = {
   REJECTED: -1
 };
 
+function getByStatus(request, reply, status) {
+  const Article = request.getDb().getModel('article');
+  return Article.findAll({where: {status: status}}).then(function (articles) {
+    return reply(articles);
+  });
+}
+
 module.exports = [
   {
     method: 'GET',
     path: '/article/pending',
     handler: function (request, reply) {
-      const Article = request.getDb().getModel('article');
-      return Article.findAll({ where: { status: Status.PENDING } }).then(function (articles) {
-        return reply(articles);
-      });
+      return getByStatus(request, reply, Status.PENDING);
+    }
+  },
+  {
+    method: 'GET',
+    path: '/article/accepted',
+    handler: function (request, reply) {
+      return getByStatus(request, reply, Status.ACCEPTED);
+    }
+  },
+  {
+    method: 'GET',
+    path: '/article/rejected',
+    handler: function (request, reply) {
+      return getByStatus(request, reply, Status.REJECTED);
     }
   }
 ];
