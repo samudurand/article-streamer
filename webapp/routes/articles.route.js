88aad564a246e2e79a0e9c2e527199b2b0bea932
samudurand <samueld@cakesolutions.net>
2016-11-25 13:22:44 +0000

    udpate status request

--- webapp/routes/articles.route.js
+++ webapp/routes/articles.route.js
@@ -1,3 +1,5 @@
+const Joi = require('joi');
+const ARTICLE_MODEL = 'article';
 const Status = {
   PENDING: 0,
   ACCEPTED: 1,
@@ -5,7 +7,7 @@ const Status = {
 };
 
 function getByStatus(request, reply, status) {
-  const Article = request.getDb().getModel('article');
+  const Article = request.getDb().getModel(ARTICLE_MODEL);
   return Article.findAll({where: {status: status}}).then(function (articles) {
     return reply(articles);
   });
@@ -31,6 +33,40 @@ module.exports = [
     path: '/article/rejected',
     handler: function (request, reply) {
       return getByStatus(request, reply, Status.REJECTED);
+    }
+  },
+  {
+    method: 'PUT',
+    path: '/article/{id}/status/{status}',
+    handler: function (request, reply) {
+      const Article = request.getDb().getModel(ARTICLE_MODEL);
+      Article.update(
+        { status: request.params.status },
+        { where: {id: request.params.id }}
+      ).then(
+        (count) => {return reply('success');},
+        (err) => {return ('error');}
+      );
+
+      // return Article.find({ where: { title: 'aProject' } })
+      //   .on('success', function (project) {
+      //     // Check if record exists in db
+      //     if (project) {
+      //       project.updateAttributes({
+      //         title: 'a very different title now'
+      //       })
+      //         .success(function () {})
+      //     }
+      //   });
+
+    },
+    config: {
+      validate: {
+        params: {
+          id: Joi.string().guid(),
+          status: Joi.number().integer().min(-1).max(1)
+        }
+      }
     }
   }
 ];
