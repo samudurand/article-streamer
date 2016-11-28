8261b60f824564ea764f4eaaf24e10fc238bd672
samudurand <samueld@cakesolutions.net>
2016-11-28 16:37:37 +0000

    display basic data

--- webapp-frontend/src/components/Accepted.vue
+++ webapp-frontend/src/components/Accepted.vue
@@ -1,4 +1,4 @@
-<template>
+<template xmlns:v-bind="http://www.w3.org/1999/xhtml">
 
   <div class="mdl-grid">
 
@@ -15,13 +15,13 @@
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td class="mdl-data-table__cell--non-numeric">2016-11-24</td>
+          <tr v-for="article in articles">
+            <td class="mdl-data-table__cell--non-numeric">{{article.publicationDate}}</td>
             <td class="mdl-data-table__cell--non-numeric">
-              <div style="word-break: break-all; white-space: normal;">#apache #Spark #MachineLearning Tutorial  #Analytics vía @BigDataReport_ https://t.co/G1YvU9dmq8 https://t.co/lj0PdRvA9B</div></td>
-            <td>100</td>
+              <div style="word-break: break-all; white-space: normal;">{{article.content}}</div></td>
+            <td>{{article.score}}</td>
             <td class="tab-logo">
-              <a href="https://twitter.com/any/status/801807662388219904"><img src="../assets/twitter.png"></a>
+              <a v-bind:href="'https://twitter.com/any/status/' + article.originalId"><img src="../assets/twitter.png"></a>
             </td>
           </tr>
         </tbody>
@@ -39,7 +39,28 @@
   export default {
     name: 'pending',
     data() {
-      return {};
+      return {
+          articles: [
+            {
+              "id": "b84802a4-ba1b-49eb-b995-bf8a7c318a10",
+              "originalId": "801568316779274240",
+              "publicationDate": "2016-11-23T00:00:00.000Z",
+              "content": "#spark teststest https://t.co/WLUWq7roAm",
+              "author": 1935423961,
+              "score": 0,
+              "status": 0
+            },
+            {
+              "id": "5c0a28cf-d154-43a5-8a9b-7acf5e840c9c",
+              "originalId": "801807662388219904",
+              "publicationDate": "2016-11-24T00:00:00.000Z",
+              "content": "#apache #Spark #MachineLearning Tutorial  #Analytics vía @BigDataReport_ https://t.co/G1YvU9dmq8 https://t.co/lj0PdRvA9B",
+              "author": 191537652,
+              "score": 100,
+              "status": 0
+            }
+          ]
+      };
     },
   };
 </script>
