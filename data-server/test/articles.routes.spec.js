2e483d5c22bd2e9f3407f3312cfb402ec4170f21
samudurand <samueld@cakesolutions.net>
2017-01-10 23:01:04 +0000

    test delete endpoint

--- data-server/test/articles.routes.spec.js
+++ data-server/test/articles.routes.spec.js
@@ -32,7 +32,7 @@ describe('Articles API', function () {
     });
   });
 
-  describe('Status', function () {
+  describe('Managed DB', function () {
 
     before(function (done) {
       cleanupDb(done);
@@ -48,7 +48,7 @@ describe('Articles API', function () {
 
     afterEach(function (done) {
       // If database name was changed to provoke database errors resets it
-      fs.access(originalDBName, fs.constants.F_OK, (err) => {
+      fs.access(tempDBName, fs.constants.F_OK, (err) => {
         if (!err) {
           fs.rename(tempDBName, originalDBName, () => {
             cleanupDb(done);
@@ -60,76 +60,69 @@ describe('Articles API', function () {
 
     });
 
-    it('gets all pending articles', (done) => {
-      server.inject({method: 'GET', url: '/article/pending'}, function (response) {
-        try {
-          response.statusCode.should.equal(200);
-          const results = response.result;
-          results.should.have.length(2);
-          results[0].id.should.equal(articlePending1.id);
-          results[1].id.should.equal(articlePending2.id);
-          done();
-        } catch (err) {
-          done(err);
-        }
-      });
-    });
+    describe('Status', function () {
 
-    it('gets all accepted articles', (done) => {
-      server.inject({method: 'GET', url: '/article/accepted'}, function (response) {
-        try {
-          response.statusCode.should.equal(200);
-          const results = response.result;
-          results.should.have.length(2);
-          results[0].id.should.equal(articleAccepted2.id);
-          results[1].id.should.equal(articleAccepted1.id);
-          done();
-        } catch (err) {
-          done(err);
-        }
+      it('gets all pending articles', (done) => {
+        server.inject({method: 'GET', url: '/article/pending'}, function (response) {
+          try {
+            response.statusCode.should.equal(200);
+            const results = response.result;
+            results.should.have.length(2);
+            results[0].id.should.equal(articlePending1.id);
+            results[1].id.should.equal(articlePending2.id);
+            done();
+          } catch (err) {
+            done(err);
+          }
+        });
       });
-    });
 
-    it('gets all rejected articles', (done) => {
-      server.inject({method: 'GET', url: '/article/rejected'}, function (response) {
-        try {
-          response.statusCode.should.equal(200);
-          const results = response.result;
-          results.should.have.length(2);
-          results[0].id.should.equal(articleRejected2.id);
-          results[1].id.should.equal(articleRejected1.id);
-          done();
-        } catch (err) {
-          done(err);
-        }
+      it('gets all accepted articles', (done) => {
+        server.inject({method: 'GET', url: '/article/accepted'}, function (response) {
+          try {
+            response.statusCode.should.equal(200);
+            const results = response.result;
+            results.should.have.length(2);
+            results[0].id.should.equal(articleAccepted2.id);
+            results[1].id.should.equal(articleAccepted1.id);
+            done();
+          } catch (err) {
+            done(err);
+          }
+        });
       });
-    });
 
-    it('change status from pending to accepted', (done) => {
-      server.inject({method: 'PUT', url: '/article/' + articlePending1.id + '/status/1'}, function (response) {
-        try {
-          response.statusCode.should.equal(204);
-          done();
-        } catch (err) {
-          done(err);
-        }
+      it('gets all rejected articles', (done) => {
+        server.inject({method: 'GET', url: '/article/rejected'}, function (response) {
+          try {
+            response.statusCode.should.equal(200);
+            const results = response.result;
+            results.should.have.length(2);
+            results[0].id.should.equal(articleRejected2.id);
+            results[1].id.should.equal(articleRejected1.id);
+            done();
+          } catch (err) {
+            done(err);
+          }
+        });
       });
-    });
 
-    it('fail to change status for non existing article', (done) => {
-      server.inject({method: 'PUT', url: '/article/00000000-0000-0000-0000-000000000000/status/1'}, function (response) {
-        try {
-          response.statusCode.should.equal(500);
-          done();
-        } catch (err) {
-          done(err);
-        }
-      });
-    });
-
-    it('fail to change status if database unreachable', (done) => {
-      fs.rename(originalDBName, tempDBName, () => {
+      it('change status from pending to accepted', (done) => {
         server.inject({method: 'PUT', url: '/article/' + articlePending1.id + '/status/1'}, function (response) {
+          try {
+            response.statusCode.should.equal(204);
+            done();
+          } catch (err) {
+            done(err);
+          }
+        });
+      });
+
+      it('fail to change status for non existing article', (done) => {
+        server.inject({
+          method: 'PUT',
+          url: '/article/00000000-0000-0000-0000-000000000000/status/1'
+        }, function (response) {
           try {
             response.statusCode.should.equal(500);
             done();
@@ -138,14 +131,74 @@ describe('Articles API', function () {
           }
         });
       });
+
+      it('fail to change status if database unreachable', (done) => {
+        fs.rename(originalDBName, tempDBName, () => {
+          server.inject({method: 'PUT', url: '/article/' + articlePending1.id + '/status/1'}, function (response) {
+            try {
+              response.statusCode.should.equal(500);
+              done();
+            } catch (err) {
+              done(err);
+            }
+          });
+        });
+      });
+
+    });
+
+    describe('Article CRUD', function () {
+
+      it('delete article', (done) => {
+        server.inject({method: 'DELETE', url: '/article/' + articlePending1.id}, function (response) {
+          try {
+            response.statusCode.should.equal(204);
+
+            const Article = sequelize.model('article');
+            Article.findAll({where: {status: 0}}).then((articles) => {
+              articles.should.have.length(1);
+              done();
+            });
+          } catch (err) {
+            done(err);
+          }
+        });
+      });
+
+      it('fail to delete not existing article', (done) => {
+        server.inject({method: 'DELETE', url: '/article/00000000-0000-0000-0000-000000000000'}, function (response) {
+          try {
+            response.statusCode.should.equal(500);
+
+            const Article = sequelize.model('article');
+            Article.findAll({where: {status: 0}}).then((articles) => {
+              articles.should.have.length(2);
+              done();
+            });
+          } catch (err) {
+            done(err);
+          }
+        });
+      });
+
+      it('fail to delete not existing article', (done) => {
+        fs.rename(originalDBName, tempDBName, () => {
+          server.inject({method: 'DELETE', url: '/article/' + articlePending1.id}, function (response) {
+            try {
+              response.statusCode.should.equal(500);
+              done();
+            } catch (err) {
+              done(err);
+            }
+          });
+        });
+      });
     });
 
     function cleanupDb(callback) {
       const Article = sequelize.model('article');
       Article.truncate().then(() => callback(), () => callback());
     }
-
   });
-
 });
 
